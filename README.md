0e8c84e06d8f2c93e30847cabbe4570dcd8b990f
Colin Kiama <colinkiama@gmail.com>
2021-03-20 23:34:41 +0000

    Handle authentication

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Mastotunes
 Let Mastodon know what music you listen too!
+
+## Prerequisites
+- [Python 3.8](python_link)
+- [pipenv](pipenv_link) - Package and environment management
+
+## Getting Started
+### Clone the project
+```bash
+git clone https://github.com/colinkiama/mastotunes.git
+```
--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from mastodon import Mastodon
 from dotenv import dotenv_values
+from pathlib import Path
 
-config = dotenv_values(".env");
-print(config)
+config = dotenv_values(".env")
 
-Mastodon.create_app(
-     config["APPLICATION_NAME"],
-     api_base_url = config["INSTANCE_ADDRESS"],
-     to_file = 'mastotunes_clientcred.secret'
-)
 
-mastodon = Mastodon(
-    client_id = 'mastotunes_clientcred.secret',
-    api_base_url = config["INSTANCE_ADDRESS"]
-)
+def login():
+    mastodon = Mastodon(client_id='mastotunes_clientcred.secret',
+                        api_base_url=config["INSTANCE_ADDRESS"])
+    mastodon.log_in(config["INSTANCE_EMAIL"],
+                    config["INSTANCE_PASSWORD"],
+                    to_file='mastotunes_usercred.secret')
+    return mastodon
 
-mastodon.log_in(
-    config["INSTANCE_EMAIL"],
-    config["INSTANCE_PASSWORD"],
-    to_file = 'mastotunes_usercred.secret'
-)
 
-mastodon.toot(f'{config["APPLICATION_NAME"]} is ready!')
+def setup_mastodon():
+    print(config)
+
+    mastodon = None
+    does_client_credentials_path_exist = Path(
+        "mastotunes_clientcred.secret").exists()
+    does_user_credentials_path_exist = Path(
+        "mastotunes_usercred.secret").exists()
+
+    if not does_client_credentials_path_exist:
+        Mastodon.create_app(config["APPLICATION_NAME"],
+                            api_base_url=config["INSTANCE_ADDRESS"],
+                            to_file='mastotunes_clientcred.secret')
+        mastodon = login()
+
+    elif not (does_user_credentials_path_exist):
+        mastodon = login()
+
+    else:
+        mastodon = Mastodon(access_token='mastotunes_usercred.secret',
+                            api_base_url=config["INSTANCE_ADDRESS"])
+    return mastodon
+
+
+mastodon = setup_mastodon()
+mastodon.toot(f'Hey from {config["APPLICATION_NAME"]}')
